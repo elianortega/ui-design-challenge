77fb7c2d21418cce25fce29918d473c819283099
elian-ortega <elian.ortega.ucr@gmail.com>
2020-07-24 21:58:58 -0600

    Week7 - Update User Icon

--- Week7_Medical/lib/screens/home_screen.dart
+++ Week7_Medical/lib/screens/home_screen.dart
@@ -24,7 +24,11 @@ class HomeScreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(right: 20.0),
             child: CircleAvatar(
-              backgroundColor: Colors.red,
+              backgroundColor: kBlue,
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
             ),
           ),
         ],
